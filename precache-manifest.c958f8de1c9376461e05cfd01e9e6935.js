d4ea25c6ba3df45306b591e7c0cee8401c25c02f
rgdzv <r_gadzhiomarov@mail.ru>
2020-08-20 12:54:59 +0300

    Updates

---
+++ precache-manifest.c958f8de1c9376461e05cfd01e9e6935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f322d9f5c96e2d8b2d015e1824c502",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "34e38b554f49ce8de540",
+    "url": "/To-do-list/static/css/main.f0a1457b.chunk.css"
+  },
+  {
+    "revision": "ccc586a65c5671f075f8",
+    "url": "/To-do-list/static/js/2.0a49e803.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/To-do-list/static/js/2.0a49e803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e38b554f49ce8de540",
+    "url": "/To-do-list/static/js/main.40835cf6.chunk.js"
+  },
+  {
+    "revision": "d07ab223c9b45d56c372",
+    "url": "/To-do-list/static/js/runtime-main.d966c86c.js"
+  },
+  {
+    "revision": "0d136ccd9341adf009d02ecb896e2f8e",
+    "url": "/To-do-list/static/media/background.0d136ccd.jpg"
+  }
+]);
